c391d0e438d37803a49398d9933a453cd0ba9d62
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-15 22:05:12 +0530

    Create 07 comprhension in python.py

---
+++ Module 1/List or Array/07 comprhension in python.py
@@ -0,0 +1,14 @@
+l1 = [x for x in range(11) if x%2 ==0]
+print(l1)
+l2 = [x for x in range(11) if x%2 != 0]
+print(l2)
+
+#alternate codes 
+l1 = []
+for x in range(11):
+  if x 52==0:
+    l1.appendex(x)
+l2 = []
+for x in range(11):
+  if x%2 != 0:
+    l2.append(x)
