c7cf9dacdd00cc56343e31ecead7e6ec985a152f
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-15 22:27:44 +0530

    Update 07 comprhension in python.py

--- Module 1/List or Array/07 comprhension in python.py
+++ Module 1/List or Array/07 comprhension in python.py
@@ -38,3 +38,22 @@ l3 = [ x for x in l2 if x.startswith("g")]
 print(l3)
 l4 = [x*2 for x in range(6)]
 print(l4)
+
+#set comprehensions in python
+
+l = [10,20,3,4,10,20,7,3]
+s1 = [x for x in l if x%2 == 0]
+s2 = [x for x in l if x%2!=0 ]
+print(s1)
+print(s2)
+
+l1 = [1,3,4,2,5]
+d1 = {x:x**3 for x in l1}
+print(d1)
+
+d2 = {x:f"ID{x} for x in range(5)}
+print(d2)
+
+l2 = [101,103,102]
+l3 = ["gfg","ide","courses"]
+d3 = {l2[i]:l3[i] for i in range (len(l2))}
