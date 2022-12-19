36b0a3e76175525fee50d1e7703e9e99f8e5a59c
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-19 13:36:33 +0530

    Update 09 second largest.py

--- Module 1/List or Array/09 second largest.py
+++ Module 1/List or Array/09 second largest.py
@@ -16,3 +16,18 @@ def getMax(l):
   for i in range(1,len(l)):
     res = max(res,l[i])
   return res
+
+# efficient solution
+def getSecMax(l):
+  if len(l)<=1:
+     return None
+     lar = l[0]
+     slar = None
+  for x in l[1:]:
+     if x > lar:
+       slar = lar
+       lar = x
+     elif x! = lar:
+       if slar==None or slar<x:
+       slar=x
+  return slar
