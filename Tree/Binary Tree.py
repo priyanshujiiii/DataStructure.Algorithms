943efd49c23a6f5f1b6d64b4af8eebb17e864d7e
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-19 17:51:00 +0530

    Create Binary Tree.py

---
+++ Tree/Binary Tree.py
@@ -0,0 +1,10 @@
+class Node:
+  def __init__(self,k):
+    self.left=None
+    self.right = None
+  self.key = k
+  
+#driver code
+root.left = Node(10)
+root.right = Node(30)
+root.left.left=Node(40)
