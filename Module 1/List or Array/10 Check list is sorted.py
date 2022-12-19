f52a17d21aced3365f005df36d87913872b61a65
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-19 14:29:53 +0530

    Create 10 Check list is sorted.py

---
+++ Module 1/List or Array/10 Check list is sorted.py
@@ -0,0 +1,10 @@
+#Here the problem is that check whether the following list is sorted or not.
+
+def listissorted(l):
+  y = "No"
+  for i in range(len(l)):
+    if l[i]==l[i+1]:
+      y = "Yes"
+    else:
+      y = "No"
+  return "No"
