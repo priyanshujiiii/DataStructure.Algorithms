ae3528576143e17531dd569d61c57d2140fb0794
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-20 00:34:02 +0530

    Create 01 implementation of linked list.py

---
+++ Module 2/Linkedlist/01 implementation of linked list.py
@@ -0,0 +1,11 @@
+class Node:
+  def __init__(self,k):
+    self.key = k
+    self.next = None
+temp1 = Node(10)
+temp2 = Node(20)
+temp3 = Node(30)
+
+temp1.next = temp2
+temp2.next = temp3
+head = temp1
