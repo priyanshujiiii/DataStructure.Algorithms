7f6f5bdd95a7a1878a45238cb6a9f34667457719
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-20 14:23:16 +0530

    Update 10 check the anagram.py

--- Module 1/3) String/10 check the anagram.py
+++ Module 1/3) String/10 check the anagram.py
@@ -4,3 +4,15 @@ def areanagram(s1,s2):
   s1 = sorted(s1)
   s2 = sorted(s2)
   return(s1==s2)
+
+def areAnagram(s1,s2):
+   if len (s!)!=len(s2):
+     return False
+   count = [0]*256
+   for i in range(len(s1)):
+     count[ord(s1[i])] +=1
+     count[ord(s2[i])] -=1
+   for x in count:
+     if x! = 0:
+      return False
+   return True
