ed1ac0f6c648e38dcc468f06e496b329d5647a50
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-20 13:33:35 +0530

    Create 05 formatted string.py

---
+++ Module 1/3) String/05 formatted string.py
@@ -0,0 +1,11 @@
+name = "ABC"
+course = "Python Course"
+s = "welcome {0} to the {1}".format(name,course)
+
+k = f"welcome {name} to the {course}"
+
+#f-string example
+s1 = "ABC"
+s2 = "abc"
+print(f"lower case of {s1} is {s1.lower())
+print(f"upper case of {s2} is {s2.lower())
