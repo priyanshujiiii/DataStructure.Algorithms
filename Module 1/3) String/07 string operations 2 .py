0caa4f97ccdb9027c74deac1e9cc17ebd27b42f8
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-20 13:55:23 +0530

    Create 07 string operations 2 .py

---
+++ Module 1/3) String/07 string operations 2 .py
@@ -0,0 +1,13 @@
+s1 = "geeks"
+print(s1)
+s2 = s1.upper()
+print(s2)
+s3 = s2.lower()
+print(s3)
+print(s1.islower())
+print(s2.isupper())
+s = "geeksforgeeks python course"
+print(s.startwith("geeks"))
+print(s.endswith("course"))
+print(s.startwith("Geeks",1))
+print(s.startwith("geeks",8,len(s1))
